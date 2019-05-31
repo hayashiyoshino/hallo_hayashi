15025aeaee956f2f5e7c3ea11273562272a7e9bf
林佳志乃 <twinklestaryoshino@gmail.com>
2019-05-31 23:03:38 +0900

    バージョン変更&修正

--- lib/hello_hayashi.rb
+++ lib/hello_hayashi.rb
@@ -2,9 +2,7 @@ require "hello_hayashi/version"
 
 module HelloHayashi
   class Error < StandardError; end
-  class Hayashi
-    def greets
-      puts 'Hello, Hayashi!'
-    end
+  def greets
+    puts 'Hello, Hayashi!'
   end
 end
